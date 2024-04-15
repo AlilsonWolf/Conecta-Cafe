c4be5d7fa10bfacae8e5e91759a8411be18c3d3b
AlilsonWolf <techangel421@gmail.com>
2024-04-15 16:41:54 -0300

    Criado a Aba Produto e suas Categorias

---
+++ ConectaCafe/wwwroot/assets/js/site.js
@@ -0,0 +1,8 @@
+let elements = document.querySelectorAll('input[type="text"]');
+elements.forEach((elem,index,array)=>{
+    
+    if(elem.value != ""){
+        elem.parentElement.classList.add('is-filled');
+    }
+
+})
